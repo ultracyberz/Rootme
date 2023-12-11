aa6a3918cd416e139ebd13b482d921cd35346d81
ultracyberz <149968539+ultracyberz@users.noreply.github.com>
2023-12-11 18:08:14 +0300

    Add files via upload
    pkg update 
    pkg upgrade
    git clone https://github.com/ultracyberz/Rootme.git
    cd Rootme
    chmod +x setup.sh
    ./setup
    
    
    Only on Termux

---
+++ rootme.sh
@@ -0,0 +1,12 @@
+echo -e "\e[31m
+
+▒█▀▀█ █▀▀█ █▀▀█ ▀▀█▀▀ █▀▄▀█ █▀▀ 
+▒█▄▄▀ █░░█ █░░█ ░░█░░ █░▀░█ █▀▀ 
+▒█░▒█ ▀▀▀▀ ▀▀▀▀ ░░▀░░ ▀░░░▀ ▀▀▀
+          	[Thomas Genenew]
+
+[+] You Are Now Rooted Feel Free to use!!!
+
+\e[0m"
+
+proot-distro login debian
---
+++ setup.sh
@@ -0,0 +1,9 @@
+clear
+pkg update
+pkg upgrade
+pkg install proot-distro
+proot-distro install debian
+chmod +x rootme.sh
+echo -e "\e[32m Why You Don't Subscribe my Youtube?\e[0m"
+termux-open https://www.youtube.com/@CyberGash
+./rootme.sh
