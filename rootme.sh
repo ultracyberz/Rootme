9e5c824bd6696f4f41d394c4ce6f059ece23f4e5
ultracyberz <149968539+ultracyberz@users.noreply.github.com>
2023-12-11 20:18:50 +0300

    rootme.sh

--- rootme.sh
+++ rootme.sh
@@ -7,9 +7,11 @@ echo -e "\e[31m
           	[Thomas Genenew]
 
 [+] You Are Now Rooted Feel Free to use!!!
-
 \e[0m"
-termux-open https://www.youtube.com/@CyberGash
+echo -e "\e[32m [•] Type exit if you want to exit Rootme.\e[0m"
 
 proot-distro login debian
 
+if [ "$1" == "exit" ]; then
+    xdg-open https://www.youtube.com/@CyberGash
+fi
