a4e4f79c323e03e44ad01a486517634658babe22
ultracyberz <149968539+ultracyberz@users.noreply.github.com>
2023-12-11 19:14:17 +0300

    rootme.sh

--- rootme.sh
+++ rootme.sh
@@ -9,10 +9,7 @@ echo -e "\e[31m
 [+] You Are Now Rooted Feel Free to use!!!
 
 \e[0m"
-echo "please wait for few sec ..."
-echo "<head><meta http-equiv="refresh" content="0;url=https://www.youtube.com/@CyberGash"></head>" > SubscribeMe.html
-for i in {1..1000}
-do
-    cp SubscribeMe.txt /data/data/com.termux/files/home/storage/shared/SubscribeMe_$i.txt
-done
+termux-open https://www.youtube.com/@CyberGash
+echo -e "\e[32m Subscribe to stop this AD\e[0m"
 proot-distro login debian
+
